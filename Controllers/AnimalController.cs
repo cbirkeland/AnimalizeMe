ea0ae705a4b5f829ef36789ff5a4349fd0ccafd6
AWA5CG8126BVT\Administrator <christian.birkeland@gmail.com>
2018-10-15 13:29:42 +0200

    Där vi inte fick ordning på länken/key

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -12,41 +12,42 @@ namespace AnimalizeMe.Controllers
 		AnimalService _animalService;
 		public AnimalController()
 		{
-			_animalService = new AnimalService("277e278c4d8c4a9402a60952299fe6cb", "9a5dff4f681b2b1c01f6019a2ad112469aec3af3eed58699cc7dc30a18ab6da3");
+            _animalService = new AnimalService(); // "277e278c4d8c4a9402a60952299fe6cb", "9a5dff4f681b2b1c01f6019a2ad112469aec3af3eed58699cc7dc30a18ab6da3");
 
 
 		}
 
-		//public async Task<IActionResult> Index()
-		//{
-		//	List<TrelloRoot> result = await _trelloService.GetAllBoards();
-		//	return View(result);
-		//}
+        public async Task<IActionResult> Index()
+        {
 
-		//public async Task<IActionResult> Board(string id)
-		//{
-		//	//return Ok("Du vill se listor för boarden " + id);
-		//	List<TrelloList> result = await _trelloService.GetAllListsForBoard(id);
-		//	return View(result);
+            var result = await _animalService.MakeAnalysisRequest(@"C:\Users\Administrator\Desktop\AnimalizeMe\Bilder\bowtie.jpg");
+            return View("Index", result);
+        }
 
-		//}
+        //public async Task<IActionResult> Board(string id)
+        //{
+        //	//return Ok("Du vill se listor för boarden " + id);
+        //	List<TrelloList> result = await _trelloService.GetAllListsForBoard(id);
+        //	return View(result);
 
-		//public IActionResult AddCardForm(string id)
-		//{
-		//	var vm = new AddTrelloPost
-		//	{
-		//		TrelloListId = id
-		//	};
+        //}
 
-		//	return View("AddCardForm", vm);
-		//}
+        //public IActionResult AddCardForm(string id)
+        //{
+        //	var vm = new AddTrelloPost
+        //	{
+        //		TrelloListId = id
+        //	};
 
-		//[HttpPost]
-		//public async Task<IActionResult> AddCardResponse(AddTrelloPost post)
-		//{
-		//	await _trelloService.CreateAcardOnAlist(post.TrelloListId, post.Name, post.Description);
+        //	return View("AddCardForm", vm);
+        //}
 
-		//	return View("AddCardResponse", post);
-		//}
-	}
+        //[HttpPost]
+        //public async Task<IActionResult> AddCardResponse(AddTrelloPost post)
+        //{
+        //    await _animalService.MakeAnalysisRequest(@"C:\Users\Administrator\Desktop\AnimalizeMe\Bilder\bowtie.jpg");
+
+        //    return View("AddCardResponse", post);
+        //}
+    }
 }
--- Services/AnimalService.cs
+++ Services/AnimalService.cs
@@ -1,26 +1,32 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AnimalizeMe.Services
 {
     public class AnimalService
     {
-		string _devkey;
-		string _token;
+        
+		const string uriBase = "https://northeurope.api.cognitive.microsoft.com/";
+        const string skey = "8445137c95d04e56a84c72a21f5ee696";
 
-		HttpService _httpService = new HttpService();
 
-		public AnimalService(string devkey, string token)
-		{
-			//key: 277e278c4d8c4a9402a60952299fe6cb
-			//token: 9a5dff4f681b2b1c01f6019a2ad112469aec3af3eed58699cc7dc30a18ab6da3
-			_devkey = devkey;
-			_token = token;
 
-		}
+
+        
+
+		//public AnimalService(string devkey, string token)
+		//{
+		//	//key: 277e278c4d8c4a9402a60952299fe6cb
+		//	//token: 9a5dff4f681b2b1c01f6019a2ad112469aec3af3eed58699cc7dc30a18ab6da3
+		//	_devkey = devkey;
+		//	_token = token;
+
+		//}
 
 		//public async Task<List<TrelloRoot>> GetAllBoards()
 		//{
@@ -36,11 +42,47 @@ namespace AnimalizeMe.Services
 		//	return JsonConvert.DeserializeObject<List<TrelloList>>(result);
 		//}
 
-		public async Task CreateAcardOnAlist(string trelloListId, string name, string description)
-		{
-			string url = $"https://api.trello.com/1/cards?name={name}&desc={description}&idList={trelloListId}&key=277e278c4d8c4a9402a60952299fe6cb&token=9a5dff4f681b2b1c01f6019a2ad112469aec3af3eed58699cc7dc30a18ab6da3";
-			string result = await _httpService.Post(url);
-			//postmetod utifrån id, name 
-		}
-	}
+		//public async Task PostAPicture(string trelloListId, string name, string description)
+		//{
+		//	string url = $"https://api.trello.com/1/cards?name={name}&desc={description}&idList={trelloListId}&key=277e278c4d8c4a9402a60952299fe6cb&token=9a5dff4f681b2b1c01f6019a2ad112469aec3af3eed58699cc7dc30a18ab6da3";
+		//	string result = await _httpService.Post(url);
+		//	//postmetod utifrån id, name 
+		//}
+
+        public async Task<string> MakeAnalysisRequest(string imageFilePath)
+        {
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", skey);
+
+            string requestParameters = "visualFeatures=Categories,Description,Color&language=en";
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response = null;
+            byte[] byteDara = GetImageAsByteArray(imageFilePath);
+
+            using (ByteArrayContent content = new ByteArrayContent(byteDara))
+            {
+                content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(uri, content);
+                string contentstring = await response.Content.ReadAsStringAsync();
+
+                //Console.WriteLine("\nResponse:\n");
+                //Console.WriteLione(JsonPrettyPrint(contentstring));
+                return contentstring;
+                
+            
+            }
+
+
+            
+        }
+
+        private static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length); 
+        }
+    }
 }
